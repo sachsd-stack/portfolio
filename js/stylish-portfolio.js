00971df590768fcc7936b252d876c9f2c6a7a576
sachsd <sachsd1116@outlook.com>
2020-11-05 15:21:23 -0500

    Added JavaScript Object Array to make adding new Projects easier

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -63,17 +63,56 @@ var onMapClickHandler = function(event) {
 // Enable map zooming with mouse scroll when the user clicks the map
 $('.map').on('click', onMapClickHandler);
 
-    // $( window ).resize(function() {
-    //     if($(window).width() >=768) {
-    //         $('#jjDesc').toggleClass(function() {
-    //           if ( $( this ).is( ".mt-5" ) ) {
-    //               console.log("class already there good to go");
-    //           } else {
-    //               $( this ).addClass(".mt-5");
-    //           }
-    //         }
-    //     }else{
-    //           $('#jjDesc').removeClass(".mt-5");
-    //     }
 
-    // });
+// Populate Project Area Javascript
+var portfolioDetails = [
+  {
+    title:'Pet Store',
+    description:'Mock Pet Store Project utilizing Bootstrap/JavaScript/jQuery',
+    image:'./img/petStore.jpg',
+    link:'https://sachsd-stack.github.io/petStore/'
+  },
+  {
+    title:'How to Cross a Piranha-Infested River',
+    description:'Madlibs with HTML,CSS, and JavaScript',
+    image:'./img/madLib.jpg',
+    link:'https://sachsd-stack.github.io/madLibs/'
+  },
+  {
+    title:'Parody Product: Buy a Piece of Stonehenge!',
+    description:'Introduction to some JavaScript, but primarily uses HTML Bootstrap and CSS',
+    image:'./img/parodyProduct.jpg',
+    link:'https://sachsd-stack.github.io/productLandingPage/'
+  },
+  {
+    title:'Javascript Bass Riff',
+    description:'Reused Given Javascript Drum Kit to a Bass Riff using Audacity/JavaScript',
+    image:'./img/jsbassriff.jpg',
+    link:'https://sachsd-stack.github.io/JS-Bass-Riff-from-Drum-Kit/'
+  },
+  {
+    title:'Standard Textile Meg Fiora Collection',
+    description:"Restyled previous developer's work to fit Interiors Collection theme. Used HTMl, CSS, JavaScript, jQuery and other scripts",
+    image:'./img/megFiora.jpg',
+    link:'http://interiors.standardtextile.com/featured/megfiora'
+  }
+]
+
+var portfolioHTML = "";
+var link = "";
+
+for(var i = 0; i < portfolioDetails.length; i++) {
+  if(i == (portfolioDetails.length - 1) && (Math.abs(i % 2) == 0)) {
+    var addCols = '<div class="col-lg-3"></div>'
+    link = addCols + '<div class="col-lg-6"><a class="portfolio-item" href="' + portfolioDetails[i].link + '" target="_blank">' + addCols;
+  }
+  else {
+    link = '<div class="col-lg-6"><a class="portfolio-item" href="' + portfolioDetails[i].link + '" target="_blank">';
+  }
+  var caption = '<div class="caption"><div class="caption-content"><div class="h2">' + portfolioDetails[i].title + '</div><p class="mb-0">' + portfolioDetails[i].description + '</p></div></div>';
+  var img = '<img class="img-fluid" src="' + portfolioDetails[i].image + '" alt="' + portfolioDetails[i].title + '"></a></div>';
+
+  portfolioHTML = portfolioHTML + link + caption + img;
+}
+
+document.getElementById('portfolioContent').innerHTML = portfolioHTML;
